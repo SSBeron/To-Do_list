0721a7171a516ada98351c354957dcf49a68178b
SSBeron <sultan.sagatov87@gmail.com>
2023-10-26 16:57:24 -0400

    first commit

---
+++ to-do_list/src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+
+  return (
+    <>
+    <main className="bg-red-800">
+      <section className="flex-1 justify-center">
+
+        <div className="">
+          <h1>To Do List</h1>
+        </div>
+
+
+        <div className="text-red-500">
+          
+          <div className="">
+            <p className="bg-white">Add note</p>
+          </div>
+          <button className="">
+            <p className="">+</p>
+          </button>
+          
+        </div>
+      </section>
+    </main>
+    </>
+  )
+}
+
+export default App
