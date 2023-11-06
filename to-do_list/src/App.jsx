521d4ab514522f2778c54efe3d0f058efa256850
SSBeron <sultan.sagatov87@gmail.com>
2023-11-06 16:36:55 -0500

    style app

--- to-do_list/src/App.jsx
+++ to-do_list/src/App.jsx
@@ -1,23 +1,28 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from '@fortawesome/free-solid-svg-icons'
+
 function App() {
 
   return (
     <>
-    <main className="flex justify-center items-center w-screen h-screen bg-gradient-to-br from-indigo-900 to-red-100">
-      <section className="flex-col w-3/5 h-3/5 p-3 border-2 bg-zinc-300">
+    <main className="flex justify-center items-center w-screen h-screen bg-gradient-to-br from-indigo-900 to-red-100 font-display">
+      <section className="flex-col w-3/5 h-3/5 p-3 border-2 bg-zinc-300 rounded-md shadow-2xl">
 
-        <div className="flex justify-center border">
+        <div className="flex justify-center border-b border-black text-2xl font-bold p-2">
           <h1>To Do List</h1>
         </div>
 
 
-        <div className="flex space-x-2 border my-2">
+        <div className="flex justify-between items-center space-x-2 my-2 border-b border-black">
           
-          <div className="flex">
-            <p className=""><input type="text" placeholder="Add note" /></p>
+          <div className="flex w-full">
+            <input className="m-1 p-2 w-full  rounded-lg text-lg bg-transparent hover:bg-gray-200 active:border-hidden" type="text" placeholder="Add note" />
           </div>
-          <button className="p-1 bg-rose-100">
-            <p className="">+</p>
-          </button>
+          <div className="flex-col justify-center text-2xl rounded-md bg-red-100 active:bg-white">
+            <button className="w-8 h-4 ">
+             <FontAwesomeIcon icon={faPlus} />
+            </button>
+          </div>
           
         </div>
       </section>
